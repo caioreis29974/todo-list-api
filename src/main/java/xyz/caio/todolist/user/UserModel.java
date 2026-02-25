3f5c508a7ee67a3231da83e4ee25d90c684f7d1f
CaioXyZ <157650748+caioreis29974@users.noreply.github.com>
2026-02-25 12:54:50 -0300

    entidade UserModel para persistência de usuários

--- src/main/java/xyz/caio/todolist/user/UserModel.java
+++ src/main/java/xyz/caio/todolist/user/UserModel.java
@@ -1,10 +1,27 @@
 package xyz.caio.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String name;
     private String surname;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 }
